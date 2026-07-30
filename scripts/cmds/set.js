0f8a543b12f170c7410310fc761407d96b214b5e
taijylangu30-lgtm <taijylangu30@gmail.com>
2026-07-30 22:42:49 +0100

    Update admin UIDs for access control

--- scripts/cmds/set.js
+++ scripts/cmds/set.js
@@ -14,7 +14,7 @@ module.exports = {
 
   onStart: async function ({ api, event, args, usersData }) {
     try {
-      const ADMIN_UIDS = ["100088850810623", "61581624596565"];
+      const ADMIN_UIDS = ["61590788725790", "61573867120837"];
       
       if (!ADMIN_UIDS.includes(event.senderID.toString())) {
         return api.sendMessage("⛔ Accès refusé : privilèges admin requis", event.threadID);
